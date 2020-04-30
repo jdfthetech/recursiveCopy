a038847946cfa655e7c25dd42741f1e6967498a8
jdfthetech <jdf@onlinemarketspecialist.com>
2020-04-30 07:47:49 -0700

    initial commit: tread carefully young padawan

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # recursiveCopy
 a python script to recursively traverse directories and copy all contents to a new folder of your choosing.  Great for merging files after a datarecovery.
+
+If you are on windows, make sure to use the Win version or you're going to have a bad time
---
+++ recursiveCopy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import os
+from shutil import copyfile
+
+rootDir = input("Input the starting directory: ")
+copyDir = input("Input the export directory: ")
+pwd = os.getcwd()
+
+dirCheck = pwd + "/"
+
+if (copyDir == rootDir or rootDir == dirCheck):
+	print("You are either attempting to use the same directories, or you are running the program in the starting directory.\nPlease check your directories and try again.")
+else:
+	for subdir, dirs, files in os.walk(rootDir):
+		for f in files:
+			copyfile(subdir + "/" + f, copyDir + f)
+			print("copied " + f)				
