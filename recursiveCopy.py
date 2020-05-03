8417021133bd5a9ec68652518a86ea7b17a04808
jdfthetech <jdf@onlinemarketspecialist.com>
2020-05-02 21:20:10 -0700

    removed windows version added windows check, merged both files, still need to test but apprently this check should be proper.  will add tkinter soon

--- recursiveCopy.py
+++ recursiveCopy.py
@@ -7,12 +7,29 @@ rootDir = input("Input the starting directory: ")
 copyDir = input("Input the export directory: ")
 pwd = os.getcwd()
 
-dirCheck = pwd + "/"
+windows = False
 
-if (copyDir == rootDir or rootDir == dirCheck):
-	print("You are either attempting to use the same directories, or you are running the program in the starting directory.\nPlease check your directories and try again.")
-else:
-	for subdir, dirs, files in os.walk(rootDir):
-		for f in files:
-			copyfile(subdir + "/" + f, copyDir + f)
-			print("copied " + f)				
+# check if windows:
+if os.name == 'nt':
+	windows == True
+
+if windows == True:
+	dirCheck = pwd + "\\"
+
+	if (copyDir == rootDir or rootDir == dirCheck):
+		print("You are either attempting to use the same directories, or you are running the program in the starting directory.\nPlease check your directories and try again.")
+	else:
+		for subdir, dirs, files in os.walk(rootDir):
+			for f in files:
+				copyfile(subdir + "\\" + f, copyDir + f)
+				print("copied " + f)				
+else:				
+	dirCheck = pwd + "/"
+
+	if (copyDir == rootDir or rootDir == dirCheck):
+		print("You are either attempting to use the same directories, or you are running the program in the starting directory.\nPlease check your directories and try again.")
+	else:
+		for subdir, dirs, files in os.walk(rootDir):
+			for f in files:
+				copyfile(subdir + "/" + f, copyDir + f)
+				print("copied " + f)				
