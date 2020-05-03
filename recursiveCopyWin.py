d25d67bc6e6c60febd798db1c3825b2a1da910d3
jdfthetech <jdf@onlinemarketspecialist.com>
2020-05-02 17:42:56 -0700

    fixed slash in win version . . . derp

--- recursiveCopyWin.py
+++ recursiveCopyWin.py
@@ -7,7 +7,7 @@ rootDir = input("Input the starting directory: ")
 copyDir = input("Input the export directory: ")
 pwd = os.getcwd()
 
-dirCheck = pwd + "/"
+dirCheck = pwd + "\\"
 
 if (copyDir == rootDir or rootDir == dirCheck):
 	print("You are either attempting to use the same directories, or you are running the program in the starting directory.\nPlease check your directories and try again.")
